6033daf61ee529252060a5519d20936a47961d37
lone0$p <loneos1337@gmail.com>
2022-09-10 17:58:43 +0200

    multiple funcs added

--- src/CsvParser/CsvParser.java
+++ src/CsvParser/CsvParser.java
@@ -1,10 +1,12 @@
 package CsvParser;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.function.Predicate;
 
 public class CsvParser implements AutoCloseable{
-    public final String SEPARATING_CHAR = ","; //Eventuellement prompt l'utilisateur sur le charactère
+    final String SEPARATING_CHAR = ","; //Eventuellement prompt l'utilisateur sur le charactère de séparation
 
     //Gestion de l'input et suivi de l'état
     private File inputCsv;
@@ -17,7 +19,8 @@ public class CsvParser implements AutoCloseable{
     private boolean readerIsAtEOF = true;
 
     //Gestion de l'input en mémoire centrale et de l'état
-    public LinkedList<String[]> importedInput;
+    public LinkedList<String[]> importedInput; // <== Pratique pour l'extraction via split, mais source de problèmes sur les dernières fonctionnalités :((
+    public LinkedList<String[]> extraBuffer;
     private boolean listHasData = false;
 
     //Gestion d'un potentiel output
@@ -63,98 +66,200 @@ public class CsvParser implements AutoCloseable{
         }
     }
     //!!!!!! Choix de conception : on débute a 0
-    //Lecture d'une ligne du CSV dans la version en mémoire
+
     public String[] extractLine(int line) {
-        String[] res = {"-1"};
         if (listHasData){
+            String[] res = {};
             if (line < importedInput.size()){
                 res = importedInput.get(line);
             }
+            return res;
         }
-        return res;
+        return null;
     }
 
     public String[] extractColumn(int col) {
-        String[] res = new String[nbLines];
-        int cpt = 0 ;
         if (listHasData){
             if (col < nbCols){
+                String[] res = new String[nbLines];
+                int cpt = 0 ;
                 for (String[] line: importedInput) {
                     res[cpt] = line[col];
                     cpt++;
                 }
+//                System.out.println("Extracted Column !");
+                return res;
             }
         }
-        System.out.println("Extracted Column !");
-        return res;
+        return null;
     }
-    //On considère la première ligne comme un entete sans contenu
-    //pb si jamais la colonne extraite contient des données autres que numériques :(
+    //On considère la première ligne comme un entete sans contenu <==
+    //pb si jamais la colonne extraite contient des données dont le type n'est pas uniforme (int & string par ex) :((
 
-    public double maxValueInColumn(int col){
-        double max = 0;
+    public String maxValueInColumn(int col){
         if (listHasData){
             if(col < nbCols){
                 String[] extractedColumn = extractColumn(col);
+                String max = extractedColumn[1];
                 for (int i = 1; i < extractedColumn.length ; i++){
-                    double extractedValue = Double.parseDouble(extractedColumn[i]);
-                    if (extractedValue > max)
+                    String extractedValue = extractedColumn[i];
+                    if (isSuperiorStrCompareTo(extractedValue.compareTo(max)))
                         max = extractedValue;
                 }
             }
         }
-        return max;
+        return null;
     }
 
-    public double minValueInColumn(int col) {
-        double min = 0;
+    public String minValueInColumn(int col) {
         if (listHasData){
             if(col < nbCols){
                 String[] extractedColumn = extractColumn(col);
-                for (int i = 1; i < extractedColumn.length ; i++){
-                    double extractedValue = Double.parseDouble(extractedColumn[i]);
-                    if (extractedValue < min)
-                        System.out.println("Here"); // ???????????????????? <== En enlevant ca ne marche plus ???????
-                        min = extractedValue;
+                String min = (extractedColumn[1]);
+                for (int i = 1; i < extractedColumn.length ; i++) {
+                    String cell = extractedColumn[i];
+                    if (isInferiorStrCompareTo(cell.compareTo(min))) {
+                        min = cell;
+                    }
                 }
+                return min;
             }
         }
-        return min;
+        return null;
     }
-
-    public double avgValueInColumn(int col) {
-        double avg = 0;
-        double cpt = 0;
+    ///// Uniquement sur valeurs numériques
+    public Double avgValueInColumn(int col) {
         if (listHasData) {
             if (col < nbCols) {
+                double avg = 0;
+                double cpt = 0;
                 String[] extractedColumn = extractColumn(col);
                 for (int i = 1 ; i < extractedColumn.length ; i++){
                     avg += Double.parseDouble(extractedColumn[i]);
                     cpt ++;
                 }
+                return (avg/cpt);
             }
         }
-        if (cpt == 0)
-            return 0;
-        else return (avg/cpt);
+        return null;
     }
 
-//    public String[] extractArrayOfData(int startLine, int startCol, int endLine, int endCol){
-//        LinkedList<String[]> auxData = new LinkedList<>();
-//        if (startLine > 0 && startCol > 0 && endLine < nbLines && endCol < nbCols){
+    //Choix de conception <== extraction ligne par ligne pour cohérence de la data extraite
+    //Si besoin, faire de l'extraction par colonne
+
+    //Revoir choix de conception, considérer travailler sur le format liste et exporter comme type objet !!!
+    public String[][] extractArrayOfData(int startLine, int endLine){
+        if (startLine >= 0 && endLine < nbLines){
+            String[][] extractedArray = new String[endLine - startLine + 1][nbCols];
+            int cpt = 0;
+            for (int i = startLine ; i <= endLine ; i++) {
+                extractedArray[cpt] = extractLine(i);
+                cpt++;
+            }
+            return extractedArray;
+        }
+        return null;
+    }
+
+    //Penser a convertir lignes par lignes via toString au lieu de faire de la comparaison mot par mot
+    public LinkedList<String[]> removeDuplicates() {
+        LinkedList<String[]> removedDuplicates = new LinkedList<>(importedInput);
+        for (int i = 0 ; i < removedDuplicates.size() - 1 ; i++) {
+            String[] lineToExam = removedDuplicates.get(i);
+            for (int j = i+1 ; j < removedDuplicates.size() ; j++) {
+                String[] comparedTo = removedDuplicates.get(j);
+                int toExamSize = lineToExam.length;
+                int comparedSize = comparedTo.length;
+                if (toExamSize == comparedSize) {
+                    int cpt = 0 ;
+                    boolean state = true;
+                    for (String cell: lineToExam
+                         ) {
+                        state = state && cell.equals(comparedTo[cpt]); //Utilisation des propriétés du ET logique pour
+                        cpt++;
+                    }
+                    if (state) {
+                        removedDuplicates.remove(comparedTo);
+                        j -= 1; // <== Permet de revenir en arrière sur la verification au cas ou on aurait plusieurs fois la meme instance d'un même élément
+                                //Solution dégueulasse mais marche ?? (Impact sur la compléxité négligeable)
+                    }
+                }
+            }
+        }
+        return removedDuplicates;
+    }
+
+//    public LinkedList<String[]> filterData(Predicate predicate) {
+//        LinkedList<String[]> filteredData = new LinkedList<>(importedInput);
+//        for (String[] line: filteredData
+//             ) {
+//            filteredData.remove((String[]) Arrays.stream(line).filter(predicate).toArray()); /// ????????
+//        }
+//        return filteredData;
+//        // !!! Not working : stream.filter returns les éléments vérifiés par le predicat
+//    }
+
+//    public Object line2object (int line) {
+//        if (inputLineIsValid(line)) {
 //
 //        }
 //    }
 
+
+
+    //POUR PERMETTRE L'UTILISATION D'UN BUFFER ET EVITER LA CREATION REDONDANTE EN MEMOIRE DE STRUCTURES DE DONNEES
+    //PENSER A IMPLEMENTER ET UTILISER LE BUFFER
+    private void initLocalBuffer() {
+        if(listHasData) {
+            extraBuffer = new LinkedList<>(importedInput);
+        }
+    }
+    private void emptyBuffer() {
+        for (String[] line : extraBuffer
+             ) {
+            extraBuffer.remove(line);
+        }
+    }
+
+    private void fillBuffer() {
+        for (String[] line: importedInput
+             ) {
+            extraBuffer.add(line.clone());
+        }
+    }
+
+    private void resetBuffer() {
+        emptyBuffer();
+        fillBuffer();
+    }
+
+
     //Auxilliaire
     private boolean listHasData() {
         return (importedInput.size() != 0);
+    }
+    private boolean isEqualStrCompareTo(int cmp2output) {
+        return cmp2output == 0;
+    }
+    private boolean isInferiorStrCompareTo(int cmp2output) {
+        return (cmp2output < 0);
+    }
+    private boolean isSuperiorStrCompareTo(int cmp2output) {
+        return cmp2output > 0;
     }
     private void updateNbLines(){
         nbLines = importedInput.size();
     }
     private void updateNbCols() {
         nbCols = importedInput.getFirst().length;
+    }
+
+    private boolean inputLineIsValid(int line) {
+        return (line >= 0 && line < nbLines);
+    }
+
+    private boolean inputColIsValid(int col) {
+        return (col >= 0 && col < nbCols);
     }
     @Override
     public void close() throws Exception {
--- src/CsvParser/test.java
+++ src/CsvParser/test.java
@@ -1,6 +1,73 @@
 package CsvParser;
 
+import java.io.ObjectStreamClass;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.function.Predicate;
+import java.util.random.RandomGenerator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class test {
     public static void main(String[] args) throws Exception {
+        CsvParser test = new CsvParser("C:\\Users\\jeune\\Documents\\Fac\\COO\\TPs\\CsvParser\\test.csv");
+        test.readCsvInput();
+/*        for (String str: test.extractColumn(0)
+             ) {
+            System.out.println(str);
+        }*/
+/*        String[] col = test.extractColumn(0);
+        for (String str: col
+             ) {
+            System.out.println(str);
+        }*/
+        /*System.out.println(test.minValueInColumn(0));*/
+/*        System.out.println(test.minValueInColumn(2));*/
+//        String[][] extract = test.extractArrayOfData(1,1);
+/*        for (String[] arr: extract
+             ) {
+            for (String str: arr
+                 ) {
+                System.out.print(str);
+            }
+            System.out.print("\n");
+        }*/
+        LinkedList<String[]> remDup = test.removeDuplicates();
+//        LinkedList<String[]> filteredData = test.filterData(x -> );
+        StringBuilder l1str = new StringBuilder();
+        String[] l1 = test.extractLine(0);
+        for (String cell :l1
+             ) {
+            l1str.append(cell);
+        }
+        StringBuilder l2str = new StringBuilder();
+        String[] l2 = test.extractLine(1);
+        for (String cell : l2
+        ) {
+            l2str.append(cell);
+        }
+        String l1f = l1str.toString();
+//        objBuilder.add(l1f);
+        String l2f = l2str.toString();
+        Stream.Builder<Object> objBuilder = Stream.builder();
+//        objBuilder.add(l2f);
+        objBuilder.accept(l1f + l2f);
+        Stream<Object> toz = objBuilder.build();
+        //On manipule comment un objet wshhhhhhh aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah
+//        Object toz = objBuilder.build();
+        System.out.println(toz);
+//        System.out.println(l1f);
+//        System.out.println(l2f);
+//        System.out.println(l1f.hashCode() == l2f.hashCode());
+
+//        for (String[] line: remDup
+//             ) {
+//            for (String cell: line
+//                 ) {
+//                System.out.print(cell + ";");
+//            }
+//            System.out.print("\n");
+//        }
+
     }
 }
